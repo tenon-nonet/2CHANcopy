b620a941505d05c449f5b70173afd872f1f8ec83
tenon-nonet <nonet0421@gmail.com>
2023-04-24 03:05:54 +0900

    feat:transactionを実装

--- app/functions/comment_add.php
+++ app/functions/comment_add.php
@@ -20,16 +20,27 @@ if (isset($_POST["submitButton"])) {
     if (empty($error_message)) {
         $post_date = date("Y-m-d H:i:s");
 
-        $sql = "INSERT INTO `comment` (`username`, `body`, `post_date`,`thread_id`) 
-        VALUES (:username, :body, :post_date, :thread_id);";
-        $statement = $pdo->prepare($sql);
+        //トランザクション開始 
+        $pdo-> beginTransaction();
 
-        // 値をセットする
-        $statement->bindParam(":username", $escaped["username"], PDO::PARAM_STR);
-        $statement->bindParam(":body", $escaped["body"], PDO::PARAM_STR);
-        $statement->bindParam(":post_date", $post_date, PDO::PARAM_STR);
-        $statement->bindParam(":thread_id", $_POST["threadID"], PDO::PARAM_STR);
-        $statement->execute();
+        try{
+            $sql = "INSERT INTO `comment` (`username`, `body`, `post_date`,`thread_id`) 
+            VALUES (:username, :body, :post_date, :thread_id);";
+            $statement = $pdo->prepare($sql);
+    
+            // 値をセットする
+            $statement->bindParam(":username", $escaped["username"], PDO::PARAM_STR);
+            $statement->bindParam(":body", $escaped["body"], PDO::PARAM_STR);
+            $statement->bindParam(":post_date", $post_date, PDO::PARAM_STR);
+            $statement->bindParam(":thread_id", $_POST["threadID"], PDO::PARAM_STR);
+    
+            $statement->execute();
+
+            $pdo->commit();
+        } catch (Exception $error){
+            $pdo->rollback();
+        }
+
     }
 }
 
