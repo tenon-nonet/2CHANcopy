c8bef82d11654bc39555742d1154a2c934f7d339
tenon-nonet <nonet0421@gmail.com>
2023-05-21 07:07:32 +0900

    線消した

--- app/parts/header.php
+++ app/parts/header.php
@@ -13,6 +13,5 @@
         <p class="subTitle">名前と本文は必須です！入力しないと書き込みできません！</p>
         <p class="subTitle">他の人が不快になる書き込みはやめましょう！</p>
         <p class="subTitle">基準は貴方の心と相談して下さい</p>
-        <hr>
     </div>
 </header>
