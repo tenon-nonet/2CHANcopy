992f634a14536aec4b0d261eca46c66dd836e321
tenon-nonet <nonet0421@gmail.com>
2023-05-24 17:02:43 +0900

    other:画像の追加

--- app/parts/linkIndex.php
+++ app/parts/linkIndex.php
@@ -6,15 +6,18 @@
 <body>
     <ul class="linkIndexItems">
         <li class="linkIndexItem">
+            <img class="indexImgA" src="https://nonet3.files.wordpress.com/2023/05/00005-2704192671-removebg-preview.png?w=500" alt="画像でない、、">
             <a href="/app/pages/threadPage.php">
-                Go Threads
+                →Go Threads
             </a>
         </li>
         <li class="linkIndexItem">
-            <a href="/app/pages/createThread.php">Create A Thread</a>
+            <img class="indexImgA" src="https://nonet3.files.wordpress.com/2023/05/00047-2603230323-removebg-preview.png?w=500" alt="画像でない、、">
+            <a href="/app/pages/createThread.php">→Create A Thread</a>
         </li>
         <li class="linkIndexItem">
-            <a href="/app/pages/threadSearch.php">Search A Thread</a>
+            <img class="indexImgA" src="https://nonet3.files.wordpress.com/2023/05/00004-1349217655-removebg-preview-1.png?w=500" alt="画像でない、、">
+            <a href="/app/pages/threadSearch.php">→Search A Thread</a>
         </li>
     </ul>
 </body>
