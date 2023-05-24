2fd6a20f235a865e5e9ced3e7fb43be86d28f725
tenon-nonet <nonet0421@gmail.com>
2023-05-24 16:45:33 +0900

    add:footerとlinkの作成

---
+++ app/parts/linkIndex.php
@@ -0,0 +1,20 @@
+<head>
+    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="./assets/styleSmart.css">
+</head>
+
+<body>
+    <ul class="linkIndexItems">
+        <li class="linkIndexItem">
+            <a href="/app/pages/threadPage.php">
+                Go Threads
+            </a>
+        </li>
+        <li class="linkIndexItem">
+            <a href="/app/pages/createThread.php">Create A Thread</a>
+        </li>
+        <li class="linkIndexItem">
+            <a href="/app/pages/threadSearch.php">Search A Thread</a>
+        </li>
+    </ul>
+</body>
